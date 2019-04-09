4817bb02f1b5ebb43db978a63aba95d8457a87fc
jianmao_glb <jianmao_glb@exp.com>
2019-04-09 12:24:53 +0800

    first commit

---
+++ test01/src/main/java/com/study/Hello.java
@@ -0,0 +1,11 @@
+package com.study;
+
+/**
+ * Author: oujianmao
+ * Date: 2019-04-09 11:58
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("hello ...");
+    }
+}
